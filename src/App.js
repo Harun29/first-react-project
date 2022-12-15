95331433ed7e5e6903e32b734c2903b7311edee3
Harun29 <117188375+Harun29@users.noreply.github.com>
2022-12-15 16:48:59 +0100

    checking branch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import './App.css';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const like = 50;
-  /* jsx converts everything to string (before converting to html)
-   except booleans and objects */
-  const link = "http://www.google.com";
 
   return (
     <div className="App">
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked {like} times</p>
-
-        <p>{10}</p>
-        <p>{"hello"}</p>
-        <p>{[1,2,3,4,5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google Site</a>
+        <h1>App Components</h1>
       </div>
     </div>
   );
