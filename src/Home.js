c16d17c2512d01b00ef0ebace8151f1109a9f790
Harun29 <117188375+Harun29@users.noreply.github.com>
2022-12-15 20:15:22 +0100

    click event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('hello world', e)
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('harun', e)}>Click em again</button>
         </div>
      );
 }
